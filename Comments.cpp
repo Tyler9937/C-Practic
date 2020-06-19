a06d7b415c9d7bb18de4719c490a2ad1f830e245
tyler9937 <tyler9937@users.noreply.github.com>
2020-06-19 17:18:48 -0400

    Practice Examples

---
+++ Comments.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // This is a comment
+    cout << "Hello World!";
+
+
+    cout << "Hello World!"; // This is also a comment
+
+
+    /* This is a multy
+    line comment */
+    cout << "Hello World!";
+    return 0;
+}
